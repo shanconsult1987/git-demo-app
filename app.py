0917a939e73a6fdf6613f99b21853713f2a58919
shivesi1990 <shivesi1990@gmail.com>
2026-04-16 05:57:40 +0000

    Updated UI message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello GitHub Demo 🚀"
+    return "Hello GitHub-Feature Branch Demo 🚀"
 
 if __name__ == "__main__":
     app.run(debug=True)
